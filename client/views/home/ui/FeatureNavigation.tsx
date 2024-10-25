11bcb1b15cde9160b62a2bf93eea0c8bf4d41a34
jiminseong <iamjms4237@gachon.ac.kr>
2024-10-25 16:45:40 +0900

    css:스탬프페이지퍼블리싱완료

--- client/views/home/ui/FeatureNavigation.tsx
+++ client/views/home/ui/FeatureNavigation.tsx
@@ -10,10 +10,12 @@ interface FeatureNavigationProps {}
 const FeatureNavigation: React.FC<FeatureNavigationProps> = ({}) => {
     return (
         <div className="flex h-[100%] w-[100%] gap-[0.5em] px-[0.25em] py-[1.25em]">
-            <Container className="flex h-[120px] w-[100%] cursor-pointer flex-col items-center justify-center gap-[0.5em]">
-                <Image src={stampIconUrl} alt="스탬프아이콘" />
-                <p className="text-[0.8125em] font-bold text-jorange">스탬프 투어</p>
-            </Container>
+            <Link className="w-full" href="/stamp">
+                <Container className="flex h-[120px] w-[100%] cursor-pointer flex-col items-center justify-center gap-[0.5em]">
+                    <Image src={stampIconUrl} alt="스탬프아이콘" />
+                    <p className="text-[0.8125em] font-bold text-jorange">스탬프 투어</p>
+                </Container>
+            </Link>
             <Link className="w-full" href="/sns">
                 <Container className="flex h-[120px] w-[100%] cursor-pointer flex-col items-center justify-center gap-[0.5em]">
                     <Image src={navigationIconUrl} alt="공유아이콘" />
